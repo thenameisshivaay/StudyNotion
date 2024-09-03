6a1691f535802f2b14dac711e5d5ad9917db73fa
Abhishek Dixit <153361834+thenameisshivaay@users.noreply.github.com>
2024-09-03 22:31:49 +0530

    Add files via upload

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Dashboard = () => {
   return (
     <div className='grid place-items-center text-richblack-100 text-3xl h-full'>
-      Home
+      Welcome to StudyNotion
     </div>
   )
 }
 
-export default Home
+export default Dashboard
